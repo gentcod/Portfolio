2b610f0a42c9190d3275a89d9f6345a25ca8103b
Oyefule <drelanorgent@gmail.com>
2022-07-29 07:08:49 +0100

    Javascript Update, UI update and Document Update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use scrict'
 
+import { TIME_SEC } from "./config.js";
+
 const welcomeMessage = document.querySelector('.greeting')
 
 const navigation = document.querySelector('.navigation')
@@ -16,7 +18,14 @@ const sectionAbout = document.querySelector('.section__about')
 const sectionContact = document.querySelector('.section__contact')
 const sectionExplore = document.querySelector('.section__explore')
 
-const hireMe = document.querySelector('.intro__btn--learn-more')
+const hireMe = document.querySelector('.intro__btn--hire-me');
+const LearnMore = document.querySelector('.intro__btn--learn-more');
+
+const overlay = document.querySelector('.intro__overlay');
+const intro = document.querySelector('.intro__container')
+const mailIcon = document.querySelectorAll('.email');
+
+const getIntouch = document.querySelector('.get-in-touch-form')
 
 
 /////////////////////////////////////////////////
@@ -101,3 +110,130 @@ navLink.forEach(function(nav) {
 
 // navLinkObserver.observe(sectionContact)
 // navLinkObserver.observe(sectionExplore)
+
+////////////////////////////////
+//Hire me mail
+const mailSubject = 'JOB OFFER';
+const mailBody = 'Hello Oyefule, I would like to hire you'
+const mailBodySocial = 'Hello Oyefule, I viewed your portfolio and would like to get in touch with you'
+
+
+hireMe.addEventListener('click', () => [
+  window.open(`mailto:drelanorgent@gmail.com?subject=${mailSubject}&body=${mailBody}`)
+])
+
+LearnMore.addEventListener('click', () => {
+  window.open(`./doc/Oyefule_Oluwatayo_Resume.pdf`)
+})
+
+overlay.addEventListener('mouseover', function() {
+  // console.log(this)
+  // this.style.display = 'none';
+  this.classList.add('hidden')
+});
+
+overlay.addEventListener('mouseout', function() {
+  setTimeout(()=> {
+    overlay.classList.remove('hidden');
+    // console.log('Overlay')
+  }, TIME_SEC * 1000)
+})
+
+mailIcon.forEach(icon => icon.addEventListener('click', () => {
+  window.open(`mailto:drelanorgent@gmail.com?subject=${mailSubject}&body=${mailBodySocial}`)
+}))
+
+getIntouch.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const formEmail = document.querySelector('.form-email').value;
+  const formName = document.querySelector('.form-name').value;
+  const forMsg = document.querySelector('.form-msg').value;
+
+  console.log(formEmail, formName, forMsg);
+
+  console.log(window.location)
+})
+
+const sliderBtnLeft = document.querySelector('.slider-btn--left');
+const sliderBtnRight = document.querySelector('.slider-btn--right');
+
+const slider = function() {
+
+  // const slider = document.querySelector('.slider');
+  const slides = document.querySelectorAll('.explore-website-feature-wrap');
+  const slideText = document.querySelectorAll('.explore-website-feature-desc--text');
+  const pagination = document.querySelector('.explore-website-feature-pagination');
+
+  let curSlide = 0; //for click count and current slide index
+  const maxSlide = slides.length 
+
+  //Create dots for slider
+  const createDots = function() {
+    slides.forEach(function(_, i) {
+      pagination.insertAdjacentHTML('afterBegin', `<button class="explore-website-feature-pagination-nav" data-slide='${i}'></button>`)
+    })
+  }
+
+  //Activate dot for slider
+  const activateDot = function(slide) {
+    document.querySelectorAll('.explore-website-feature-pagination-nav').forEach(dot => dot.classList.remove('explore-website-feature-pagination-nav-active'))
+    document.querySelector(`.explore-website-feature-pagination-nav[data-slide='${slide}']`).classList.add('explore-website-feature-pagination-nav-active')
+  }
+
+  //Go to slide
+  const gotoSlide = function(cur) {
+    slides.forEach((slide, i) => {
+      slide.style.transform = `translateX(${100 * (i - cur)}%)`;
+    })
+
+    slideText.forEach((text, i) => {
+      text.style.transform = `translateX(${100 * (i - cur)}%)`;
+    })
+  }
+
+  //Next slide
+  const nextSlide = function() {
+    if (curSlide === maxSlide -1) {
+      curSlide = 0; //To return to first slide
+    }
+    else curSlide++;
+
+    gotoSlide(curSlide)
+    activateDot(curSlide)
+  }
+
+  //Previous slide
+  const prevSlide = function() {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1
+    }
+    else curSlide--;
+
+    gotoSlide(curSlide)
+    activateDot(curSlide)
+  }
+
+  //Set initial position of slides or initialize functions
+  const init = function() {
+    gotoSlide(0)
+    createDots()
+    activateDot(0)
+  }
+  init()
+
+  sliderBtnRight.addEventListener('click', nextSlide)
+  sliderBtnLeft.addEventListener('click', prevSlide)
+
+  pagination.addEventListener('click', function(e) {
+    e.preventDefault()
+    
+    if (e.target.classList.contains('explore-website-feature-pagination-nav')) {
+      const {slide} = e.target.dataset //Destructuring
+      gotoSlide(slide)
+      activateDot(slide)
+    }
+  })
+}
+
+slider()
